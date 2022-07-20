9817b68b858493438d56ec8c753e787420e28d86
Jonathan Braat <jonathan.braat@live.com>
2022-07-20 22:00:44 +1200

    [ADD] mdx render

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,5 @@
 import { MDXRemote } from 'next-mdx-remote'
-
+import { serialize } from 'next-mdx-remote/serialize'
 
 export default function PostPage({ post }: { post: any }) {
   return (
@@ -7,4 +7,25 @@ export default function PostPage({ post }: { post: any }) {
       <MDXRemote {...post.content} />
     </>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = ['/test']
+  return {
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: 'blocking'
+  };
+}
+
+export async function getStaticProps() {
+  // MDX text - can be from a local file, database, anywhere
+  const source = 'Some **mdx** text, with a component'
+  const mdxSource = await serialize(source)
+  return {
+    props: {
+      post: {
+        content: mdxSource
+      }
+    }
+  }
 }
