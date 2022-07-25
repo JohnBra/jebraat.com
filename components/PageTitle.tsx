8c73e37f5dcec78afb38426d333f5b27f18607e2
Jonathan Braat <jonathan.braat@live.com>
2022-07-26 09:55:13 +1200

    [ADD] page title component

---
+++ components/PageTitle.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import cn from 'classnames'
+
+export default function PageTitle({ children, className, ...rest }: React.HTMLProps<HTMLHeadingElement>) {
+  return (
+    <h1
+      className={cn(
+        'text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14',
+         className
+       )}
+      {...rest}
+    >
+      {children}
+    </h1>
+  )
+}
