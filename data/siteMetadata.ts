7fae6c6c38d465468d35a0be3408d200f7a5f639
Jonathan Braat <jonathan.braat@live.com>
2022-07-30 23:03:49 +1200

    [ADD] seo stuff

---
+++ data/siteMetadata.ts
@@ -0,0 +1,19 @@
+const siteMetadata = {
+  title: 'John Braat - Developer amd creator',
+  author: 'Jonathan Braat',
+  language: 'en-us',
+  locale: 'en-US',
+  theme: 'system',
+  siteUrl: 'https://jebraat.com',
+  siteRepo: 'https://github.com/JohnBra/jebraat.com',
+  siteLogo: '',
+  socialBanner: '',
+  email: 'hello@jebraat.com',
+  github: 'https://github.com/JohnBra',
+  twitter: 'https://twitter.com/jebraat',
+  youtube: 'https://www.youtube.com/channel/UCSYoM12jgDhSN57CMCqr-ZA',
+  linkedin: 'https://www.linkedin.com/in/jebraat',
+  twitch: 'https://www.twitch.tv/johne_labs'
+}
+
+export default siteMetadata
