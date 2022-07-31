53ff1f377305babc50d3244a31054ba504d52470
Jonathan Braat <jonathan.braat@live.com>
2022-07-31 19:09:14 +1200

    [ADD] favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,33 +4,33 @@ export default function Document(props: any) {
   return (
     <Html lang="en">
       <Head>
-        {/*
-        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-        <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link href="/static/icons/favicon.ico" rel="shortcut icon" />
+        <link href="/static/icons/site.webmanifest" rel="manifest" />
         <link
-          href="/static/favicons/apple-touch-icon.png"
+          href="/static/icons/apple-touch-icon.png"
           rel="apple-touch-icon"
           sizes="180x180"
         />
         <link
-          href="/static/favicons/favicon-32x32.png"
+          href="/static/icons/favicon-32x32.png"
           rel="icon"
           sizes="32x32"
           type="image/png"
         />
         <link
-          href="/static/favicons/favicon-16x16.png"
+          href="/static/icons/favicon-16x16.png"
           rel="icon"
           sizes="16x16"
           type="image/png"
         />
         <link
           color="#4a9885"
-          href="/static/favicons/safari-pinned-tab.svg"
+          href="/static/icons/safari-pinned-tab.svg"
           rel="mask-icon"
         />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
+        {/*
         <meta
           content="/static/favicons/browserconfig.xml"
           name="msapplication-config"
