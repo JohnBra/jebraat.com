eaa4f9f0fa5e599ab363ab134be3b3f345e91f8b
Jonathan Braat <jonathan.braat@live.com>
2022-08-07 13:04:46 +1200

    [DEL] cleanup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,41 +1,6 @@
 import React from 'react'
-import SocialIcon from '@/components/SocialIcon'
-import { navLinks, socialLinks } from '@/lib/constants'
 import SquigglyLine from '@/components/SquigglyLine'
 
-/*
-const navigation = {
-  main: navLinks,
-  social: socialLinks,
-}
-
-export default function Footer({ className, ...rest }: React.HTMLProps<HTMLElement>) {
-  return (
-    <footer className={className} {...rest}>
-      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-          {navigation.main.map((item) => (
-            <div key={item.name} className="px-5 py-2">
-              <a href={item.href} className="text-base text-gray-500 dark:text-gray-300 hover:text-gray-900 hover:dark:text-gray-100">
-                {item.name}
-              </a>
-            </div>
-          ))}
-        </nav>
-        <div className="mt-8 flex justify-center space-x-6">
-          {navigation.social.map((item) => (
-            <SocialIcon key={item.kind} kind={item.kind} href={item.href} />
-          ))}
-        </div>
-        <div className="flex flex-col mt-8 text-center text-base text-gray-500 dark:text-gray-400">
-          <span>&copy; {` ${new Date().getFullYear()} `} Spryse Limited&trade;. All rights reserved.</span>
-          <span>Developed by Jonathan Braat</span>
-        </div>
-      </div>
-    </footer>
-  )
-}
-*/
 const navigation = {
   solutions: [
     { name: 'Blog', href: '#' },
@@ -55,10 +20,9 @@ const navigation = {
     { name: 'Press', href: '#' },
     { name: 'Partners', href: '#' },
   ],
-  social: socialLinks
 }
 
-export default function Example() {
+export default function Footer() {
   return (
     <footer className="py-12 px-4 overflow-hidden sm:px-0" aria-labelledby="footer-heading">
       <SquigglyLine className="my-8"/>
