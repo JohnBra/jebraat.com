4d3485ed4f7d1ef110c1c6677b7abd652c56ab8b
Jonathan Braat <jonathan.braat@live.com>
2022-08-07 16:15:23 +1200

    [MOD] style changes

--- components/SquigglyLine.tsx
+++ components/SquigglyLine.tsx
@@ -8,8 +8,8 @@ export default function SquigglyLine({ className }: Props) {
   return (
     <div className={classNames('relative', className)}>
       <div className={"h-2.5 w-full"}>
-        <div className="small-line small-line-1" />
-        <div className="small-line small-line-2" />
+        <div className="squiggly-line zig" />
+        <div className="squiggly-line zag" />
       </div>
     </div>
   )
