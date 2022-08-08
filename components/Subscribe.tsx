83a3b127fd208ca12870ff35d8c070eee452a9af
Jonathan Braat <jonathan.braat@live.com>
2022-08-08 21:32:45 +1200

    [MOD] newsletter style

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { useState, useRef, FormEvent } from 'react'
-import useSWR from 'swr'
-
-import fetcher from 'lib/fetcher'
-import { Form, FormState, Subscribers } from 'lib/types'
+import { Form, FormState } from 'lib/types'
 import SuccessMessage from 'components/SuccessMessage'
 import ErrorMessage from 'components/ErrorMessage'
 import LoadingSpinner from 'components/LoadingSpinner'
@@ -13,8 +10,6 @@ import CustomLink from '@/components/Link'
 export default function Subscribe() {
   const [form, setForm] = useState<FormState>({ state: Form.Initial })
   const ref = useRef<HTMLInputElement>(null)
-  const { data } = useSWR<Subscribers>('/api/newsletter/subscribers', fetcher)
-  const subscriberCount = Number(data?.count)
 
   const subscribe = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -44,11 +39,11 @@ export default function Subscribe() {
   }
 
   return (
-    <div className="border border-blue-200 rounded-md p-6 my-4 w-full dark:border-gray-800 bg-blue-50 dark:bg-gray-800">
-      <p className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
+    <div className="border border-blue-200 rounded-md p-6 my-4 w-full dark:border-gray-700 bg-blue-50 dark:bg-gray-800">
+      <p className="text-lg md:text-xl font-bold text-neutral-900 dark:text-neutral-100">
         Subscribe to the newsletter
       </p>
-      <p className="my-1 text-gray-800 dark:text-gray-200">
+      <p className="my-1 text-neutral-700 dark:text-neutral-200">
         Get emails from me about tech, travel and personal development.
       </p>
       <form className="relative my-4" onSubmit={subscribe}>
@@ -59,10 +54,11 @@ export default function Subscribe() {
           type="email"
           autoComplete="email"
           required
-          className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
+          className="px-4 py-2 mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
         />
+        {/* bg-gray-100 dark:bg-gray-700 hover:bg-sky-200 text-gray-900 dark:text-gray-100  */}
         <button
-          className="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
+          className="flex items-center justify-center absolute right-1 top-1 px-4 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-2 dark:border-gray-500 hover:border-sky-400 hover:dark:border-sky-400 rounded"
           type="submit"
         >
           {form.state === Form.Loading ? <LoadingSpinner /> : 'Subscribe'}
@@ -73,10 +69,7 @@ export default function Subscribe() {
       ) : form.state === Form.Success ? (
         <SuccessMessage>{form.message}</SuccessMessage>
       ) : (
-        <p className="text-sm text-gray-800 dark:text-gray-200">
-          {`${
-            subscriberCount > 0 ? subscriberCount.toLocaleString() : '-'
-          } subscribers – `}
+        <p className="text-sm text-neutral-500 dark:text-neutral-200 hover:text-neutral-900 hover:dark:text-neutral-50">
           <CustomLink href={siteMetadata.revue}>
             View all issues
           </CustomLink>
