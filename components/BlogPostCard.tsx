7e4dfde88c8a99b055dcac7a529db5a7883463ed
Jonathan Braat <jonathan.braat@live.com>
2022-08-10 18:24:30 +1200

    [MOD] blog post card, 26 marketing tasks article

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -23,7 +23,7 @@ export default function BlogPostCard({
       )}
     >
       <div className="h-full px-4 pt-4 pb-6 sm:p-4">
-        <div className="flex flex-col justify-between">
+        <div className="h-full flex flex-col justify-between">
           <h4 className="mb-6 w-full text-xl font-medium tracking-tight text-neutral-900 dark:text-neutral-100 sm:mb-10">
             {title}
           </h4>
