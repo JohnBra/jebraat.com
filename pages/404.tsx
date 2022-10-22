5e777e79bb9c7c1f497e56038480b400ce3c3cdc
Jonathan Braat <jonathan.braat@live.com>
2022-10-22 17:17:10 +1300

    [ADD] custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className="min-h-60 flex items-center justify-center">
+      <div className="text-2xl font-mono">
+        404 | Not found
+      </div>
+    </div>
+  )
+}
