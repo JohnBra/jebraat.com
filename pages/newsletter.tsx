f8cbab04ff236b09ce7e40d93784a6f0e4e427ca
Jonathan Braat <jonathan.braat@live.com>
2023-01-07 17:54:46 +1300

    [ADD] newsletter page

---
+++ pages/newsletter.tsx
@@ -0,0 +1,16 @@
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+
+export default function Newsletter() {
+  return (
+    <>
+      <PageSEO
+        title={`The Inspired Indiepreneur - ${siteMetadata.authorTitle}`}
+        description="Inspiring you to make your first dollar online. Every Sunday every other week you get 1 actionable tip to launch, grow and monetize your Indie business in less than 5 minutes."
+      />
+      <div>
+        hello
+      </div>
+    </>
+  )
+}
